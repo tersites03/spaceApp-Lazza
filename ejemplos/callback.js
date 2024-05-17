c34ea4d073af1a3f8e071eb3f1ee00a7c52e45d7
Jorge <tersites.03@gmail.com>
2024-05-17 08:40:18 -0300

    trbajando con callback y callback hell

--- ejemplos/callback.js
+++ ejemplos/callback.js
@@ -1,11 +1,26 @@
 
-function uno () {
+function uno (dos) {
     console.log("uno")
+    setTimeout(dos, 2000);
     
 }
 
-function dos () {
+function dos (tres) {
     console.log("dos")
+    setTimeout(tres, 2000)
     
 }
- setTimeout(uno,5000)
+
+function tres (cuatro) {
+    console.log("tres")
+    setTimeout(cuatro, 2000);
+    
+}
+
+function cuatro () {
+    console.log("cuatro")
+    
+}
+
+setTimeout(()=> uno(dos),5000);
+
